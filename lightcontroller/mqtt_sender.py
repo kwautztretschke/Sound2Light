0e5fdf22f545595ef2e0c8e557197888688eed63
Victoria Stöffler <victoria.stoeffler@tum.de>
2023-10-02 15:06:29 +0200

    implemented presets

--- lightcontroller/mqtt_sender.py
+++ lightcontroller/mqtt_sender.py
@@ -23,7 +23,6 @@ class mqtt_sender:
 		self.current_preset = self.presets[index]
 		for reactor in self.current_preset:
 			self.send_command_list(self.current_preset[reactor]["preset"], reactor)
-			self.apply_variation(0)
 
 	def apply_variation(self, index):
 		for reactor in self.current_preset:
@@ -36,8 +35,9 @@ class mqtt_sender:
   
 	reactors = [
 		"MagnatBoxen",
-		"RatDerGeleerten",
-		"tollerESP"
+		"middle",
+		"left",
+		"right"
 	]
 
 	palettes = [
@@ -46,24 +46,209 @@ class mqtt_sender:
 		]
 	]
 	presets = {
-		0: {
+		0: { # all simpleSync, Boxen on the Bass, rest on volume. Good for drops.
 			"MagnatBoxen": {
 				"preset": [
 					("focus", "simpleSync"),
 					("input/channel", "256"),
-     				("input/smooth", "5")
+     				("input/smooth", "5"),
+					("input/colorindex", "0"),
 				],
 				"variations": {
 					0: [("input/colorindex", "0")],
-					1: [("input/colorindex", "4")],
-					2: [("input/colorindex", "2")],
+					1: [("input/colorindex", "2")],
+					2: [("input/colorindex", "4")],
 					3: [("input/colorindex", "6")],
-				}
-			}
+				},
+			},
+			"middle": {
+				"preset": [
+					("focus", "simpleSync"),
+					("input/channel", "0"),
+	 				("input/smooth", "3"),
+					("input/colorindex", "1"),
+				],
+				"variations": {
+					0: [("input/colorindex", "1")],
+					1: [("input/colorindex", "3")],
+					2: [("input/colorindex", "5")],
+					3: [("input/colorindex", "7")],
+				},
+			},
+			"left": {
+				"preset": [
+					("focus", "simpleSync"),
+					("input/channel", "128"),
+	 				("input/smooth", "5"),
+					("input/colorindex", "1"),
+				],
+				"variations": {
+					0: [("input/colorindex", "1")],
+					1: [("input/colorindex", "3")],
+					2: [("input/colorindex", "5")],
+					3: [("input/colorindex", "7")],
+				},
+			},
+			"right": {
+				"preset": [
+					("focus", "simpleSync"),
+					("input/channel", "129"),
+	 				("input/smooth", "5"),
+					("input/colorindex", "1"),
+				],
+				"variations": {
+					0: [("input/colorindex", "1")],
+					1: [("input/colorindex", "3")],
+					2: [("input/colorindex", "5")],
+					3: [("input/colorindex", "7")],
+				},
+			},
 		},
-		1: [
-			
-		],
+		1: { # combination of barsync, and simple. Good for anything. Very stereo
+			"MagnatBoxen": {
+				"preset": [
+					("focus", "simpleStereoSync"),
+					("input/channel/L", "128"),
+					("input/channel/R", "129"),
+	 				("input/smooth/L", "3"),
+	 				("input/smooth/R", "3"),
+					("input/colorindex", "0"),
+					("input/colorindexoffset", "1")
+				],
+				"variations": {
+					0: [("input/colorindex", "0")],
+					1: [("input/colorindex", "2")],
+					2: [("input/colorindex", "4")],
+					3: [("input/colorindex", "6")],
+				},
+			},
+			"middle": {
+				"preset": [
+					("focus", "barFadeStereoSync"),
+					("input/channel/L", "128"),
+					("input/channel/R", "129"),
+	 				("input/smooth/L", "3"),
+	 				("input/smooth/R", "3"),
+					("input/mode", "IN"),
+					("input/colorindex", "1"),
+					("input/colorindexoffset", "-1"),
+				],
+				"variations": {
+					0: [("input/colorindex", "1")],
+					1: [("input/colorindex", "3")],
+					2: [("input/colorindex", "5")],
+					3: [("input/colorindex", "7")],
+					4: [("input/mode", "IN")],
+					5: [("input/mode", "OUT")],
+				},
+			},
+			"left": {
+				"preset": [
+					("focus", "barFadeSync"),
+					("input/channel", "128"),
+	 				("input/smooth", "5"),
+					("input/mode", "R2L"),
+					("input/colorindex", "0"),
+				],
+				"variations": {
+					0: [("input/colorindex", "0")],
+					1: [("input/colorindex", "2")],
+					2: [("input/colorindex", "4")],
+					3: [("input/colorindex", "6")],
+					4: [("input/mode", "R2L")],
+					5: [("input/mode", "L2R")],
+				},
+			},
+			"right": {
+				"preset": [
+					("focus", "barFadeSync"),
+					("input/channel", "129"),
+	 				("input/smooth", "5"),
+					("input/mode", "R2L"),
+					("input/colorindex", "1")
+				],
+				"variations": {
+					0: [("input/colorindex", "1")],
+					1: [("input/colorindex", "3")],
+					2: [("input/colorindex", "5")],
+					3: [("input/colorindex", "7")],
+					4: [("input/mode", "R2L")],
+					5: [("input/mode", "L2R")],
+				},
+			},
+		},
+		2: { # some rippling, good for calm sections
+			"MagnatBoxen": {
+				"preset": [
+					("focus", "barFadeSync"),
+					("input/channel", "256"),
+     				("input/smooth", "5"),
+				],
+				"variations": {
+					0: [("input/colorindex", "0")],
+					1: [("input/colorindex", "2")],
+					2: [("input/colorindex", "4")],
+					3: [("input/colorindex", "6")],
+				},
+			},
+			"middle": {
+				"preset": [
+					("focus", "rippleSync"),
+					("input/channel", "0"),
+	 				("input/smooth", "3"),
+					("input/mode", "IN"),
+					("input/interval", "10"),
+				],
+				"variations": {
+					0: [("input/colorindex", "1")],
+					1: [("input/colorindex", "3")],
+					2: [("input/colorindex", "5")],
+					3: [("input/colorindex", "7")],
+					4: [("input/mode", "IN")],
+					5: [("input/mode", "OUT")],
+					6: [("input/mode", "R2L")],
+					7: [("input/mode", "L2R")],
+				},
+			},
+			"left": {
+				"preset": [
+					("focus", "rippleSync"),
+					("input/channel", "0"),
+	 				("input/smooth", "5"),
+					("input/mode", "R2L"),
+					("input/interval", "10"),
+				],
+				"variations": {
+					0: [("input/colorindex", "1")],
+					1: [("input/colorindex", "3")],
+					2: [("input/colorindex", "5")],
+					3: [("input/colorindex", "7")],
+					4: [("input/mode", "R2L")],
+					5: [("input/mode", "L2R")],
+					6: [("input/mode", "R2L")],
+					7: [("input/mode", "L2R")],
+				},
+			},
+			"right": {
+				"preset": [
+					("focus", "rippleSync"),
+					("input/channel", "0"),
+	 				("input/smooth", "5"),
+					("input/mode", "R2L"),
+					("input/interval", "10"),
+				],
+				"variations": {
+					0: [("input/colorindex", "1")],
+					1: [("input/colorindex", "3")],
+					2: [("input/colorindex", "5")],
+					3: [("input/colorindex", "7")],
+					4: [("input/mode", "R2L")],
+					5: [("input/mode", "L2R")],
+					6: [("input/mode", "L2R")],
+					7: [("input/mode", "R2L")],
+				},
+			},
+		},
 	}
 
 mqtt = mqtt_sender()
